91ee1c4d45480fa4a260edec3b15d08187bc3f3e
Dima Abramov <demween@gmail.com>
2019-10-25 09:04:00 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Дима on 25.10.2019.
+//  Copyright © 2019 Dima Abramov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
